0421bc232c625431ab3e67bd273040c18c2730b8
aethrna <nogamisan5@gmail.com>
2025-12-09 23:19:53 +0700

    Fixed laravel framework configuration and output

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: false,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
